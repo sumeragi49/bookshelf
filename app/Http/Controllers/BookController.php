9ff982d788ba9c3aca45fe05d835c32219d7ba4c
sumeragi <sumeragi.0129@outlook.jp>
2026-07-31 10:05:51 +0900

    完成２

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -189,6 +189,7 @@ class BookController extends Controller
                     -> withAvg('reviews', 'rating')
                     -> withCount('reviews')
                     -> orderByDesc('reviews_avg_rating')
+                    -> take(10)
                     -> get();
 
         return view('ranking.index', compact('rankedBooks'));
